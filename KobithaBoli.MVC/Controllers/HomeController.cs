3d2796672f63fd039dffbb4bdeab16ac6f0329aa
RanjanBiswasFedJobs <ranjanbiswasfedjobs@gmail.com>
2015-02-20 14:11:44 -0500

    KobithaBoli
    Kobitha Boli MVC 4 web site.

---
+++ KobithaBoli.MVC/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace KobithaBoli.MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+
+    }
+}
